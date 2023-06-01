d2889161752ed7c94c7dea28d6ebe9fe7fe563ab
chenyinen <15757596362@163.com>
2023-06-01 20:15:41 +0800

    init

---
+++ example.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <los_memory.h>
+
+char buf[1024*1024];
+void *p[1024];
+
+int main()
+{
+    int i;
+    int num;
+    int len;
+
+    srand(time(NULL));
+    LOS_MemInit(buf, 1024*1024);
+
+    /* 随机读写测试 */
+    for(i=0; i<100000;i++) {
+        num = rand() % 1024;
+        len = rand() % 1024 + 1;
+        if (p[num] == NULL) {
+            p[num] = LOS_MemAlloc((void*)buf, len);
+            memset(p[num], 0, len);
+        }
+        else {
+            LOS_MemFree((void*)buf, p[num]);
+            p[num] = NULL;
+        }
+    }
+    printf("over\n");
+    return 0;
+}
