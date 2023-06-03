1c79896192c9c5207b54528621495c0e08f05d97
chenyinen <15757596362@163.com>
2023-06-03 15:41:31 +0800

    example

--- example.c
+++ example.c
@@ -17,15 +17,18 @@ int main()
     srand(time(NULL));
     LOS_MemInit(buf, 1024*1024);
 
+    printf("init buf address:%p\n", (void*)buf);
     /* 随机读写测试 */
-    for(i=0; i<100000;i++) {
+    for(i=0; i<1000;i++) {
         num = rand() % 1024;
         len = rand() % 1024 + 1;
         if (p[num] == NULL) {
             p[num] = LOS_MemAlloc((void*)buf, len);
+            printf("malloc:%p, len:%lu\n", (void*)p[num], (size_t)len);
             memset(p[num], 0, len);
         }
         else {
+            printf("free:%p\n", (void*)p[num]);
             LOS_MemFree((void*)buf, p[num]);
             p[num] = NULL;
         }
